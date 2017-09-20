f9b764ab3b46cdf489cc89932e340a9831ae7a57
Adam Ježek <mail@ajezek.cz>
2017-09-21 00:08:57 +0200

    x

--- source/python_service/pydrink.py
+++ source/python_service/pydrink.py
@@ -106,7 +106,9 @@ class PyDrink(threading.Thread):
         command = "m" + dir + str(abs(self.cart_position - pos))
         while 1:
             self.send_command("m" + dir + str(abs(self.cart_position - pos)))
-            rep = self.read_reply(2).replace("home","")
+            rep = self.read_reply(2)
+            if rep:
+                rep = rep.replace("home","").replace("DONE","")
             self.logger.info("sending "+command+", received "+rep)
             if rep == command:
                 break
